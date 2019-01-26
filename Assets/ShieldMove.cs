5acc5c3676fc0e0750e7830ac5659556f18b9f25
AlexandreFiale <afiale@agaetis.fr>
2019-01-26 10:36:45 +0100

    add moveSpeed and shieldControl

---
+++ Assets/ShieldMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldMove : MonoBehaviour
+{
+    public float moveSpeed = 600f;
+    float movement = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxisRaw("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.forward, movement * Time.fixedDeltaTime * -moveSpeed);
+    }
+}
