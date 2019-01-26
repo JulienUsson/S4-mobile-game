107324e571e7348221fa4978e9e2749023b6560f
Arnaud Ribeyrotte <aribeyrotte@agaetis.fr>
2019-01-26 11:52:21 +0100

    Add prebab Spawn

---
+++ Assets/Scripts/SpawnMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnMove : MonoBehaviour
+{
+    public float moveSpeed = 600f;
+    float movement = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+   
+    }
+
+    void FixedUpdate()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.forward, Time.fixedDeltaTime * -moveSpeed);
+    }
+}
