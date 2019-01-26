474a171ac01468a414bea59de99652e812e2bc4c
Julien Usson <jusson@agaetis.fr>
2019-01-26 16:12:40 +0100

    :sparkles: dialog

--- Assets/Scripts/Dialog.cs
+++ Assets/Scripts/Dialog.cs
@@ -6,10 +6,4 @@ public class Dialog
 {
     public string speaker;
     public string content;
-
-    public Dialog(string speaker, string content)
-    {
-        this.speaker = speaker;
-        this.content = content;
-    }
 }
