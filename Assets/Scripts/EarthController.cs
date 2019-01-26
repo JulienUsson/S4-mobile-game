6886245f8f80612baad88bbf2702362b90b713d2
Arnaud Ribeyrotte <aribeyrotte@agaetis.fr>
2019-01-27 00:06:07 +0100

    rerefix animation HOME

--- Assets/Scripts/EarthController.cs
+++ Assets/Scripts/EarthController.cs
@@ -17,18 +17,25 @@ public class EarthController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    IEnumerator timerWaiting()
+    {
+        yield return new WaitForSeconds(1.5f);
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        earthPV --;
+        earthPV--;
 
         anim.SetBool("Hitted", true);
 
         anim.SetInteger("PV", earthPV);
-        
+
         if (earthPV < 1)
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+
     }
 }
