d8a8ec7dc0cc4dc1789b8b821a8394ef2957dba8
Julien Usson <jusson@agaetis.fr>
2019-01-26 23:55:54 +0100

    :lipstick: startMenu

--- Assets/Scripts/QteManager.cs
+++ Assets/Scripts/QteManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class QteManager : MonoBehaviour
 {
@@ -36,6 +37,7 @@ public class QteManager : MonoBehaviour
     {
         if (keys.Count == 0)
         {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             return;
         }
 
