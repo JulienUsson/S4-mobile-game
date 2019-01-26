5108c121863065751ec6dd1e53d6dd8f3a5ce2c8
Arnaud Ribeyrotte <aribeyrotte@agaetis.fr>
2019-01-26 12:50:02 +0100

    spawn ennemies

---
+++ Assets/EnnemyMover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemyMover : MonoBehaviour
+{
+    public float movespeed = 5;
+    private Rigidbody2D rb;
+    public Transform earth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // rb.transform.Translate(0, -1, movespeed);
+        Vector3 targetDir = earth.position - transform.position;
+        rb.position = targetDir;
+    }
+}
--- Assets/Scripts/SpawnMove.cs
+++ Assets/Scripts/SpawnMove.cs
@@ -6,21 +6,22 @@ public class SpawnMove : MonoBehaviour
 {
     public float moveSpeed = 600f;
     float movement = 0f;
+    public GameObject ennemy;
 
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
-   
     }
 
     void FixedUpdate()
     {
         transform.RotateAround(Vector3.zero, Vector3.forward, Time.fixedDeltaTime * -moveSpeed);
+        Instantiate(ennemy, transform.position, transform.rotation);
+
     }
 }
