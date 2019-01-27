54868bdb3b1d60aff450bc232d18f8fdaf261b36
Julien Usson <jusson@agaetis.fr>
2019-01-27 10:22:02 +0100

    :sparkles: delta time for end screen

--- Assets/Scripts/GameOverController.cs
+++ Assets/Scripts/GameOverController.cs
@@ -6,19 +6,24 @@ using UnityEngine.SceneManagement;
 
 public class GameOverController : MonoBehaviour
 {
+    public float delta = 1f;
     public Animator earthAnimator;
     public GameObject canvas;
     public GameObject highway;
     private bool animationDone = false;
     void Update()
     {
+        if (delta > 0)
+        {
+            delta -= Time.deltaTime;
+        }
         if (!animationDone && earthAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
         {
             animationDone = true;
             canvas.SetActive(true);
             highway.SetActive(true);
         }
-        if (animationDone && Input.anyKeyDown)
+        if (animationDone && Input.anyKeyDown && delta <= 0f)
         {
             SceneManager.LoadScene(1);
             return;
