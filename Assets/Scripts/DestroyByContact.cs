111f7a974a8c630e8f851e37643993131579c651
AlexandreFiale <afiale@agaetis.fr>
2019-01-27 10:14:25 +0100

    update Explosion with sound

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour
 {
+    public GameObject Explosion;
+
     void OnTriggerEnter2D(Collider2D other)
     {
+        Instantiate(Explosion, other.transform.position, other.transform.rotation);
+
         Destroy(other.gameObject);
     }
 }
